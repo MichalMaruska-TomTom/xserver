047b811163bd90203040187dd11b25fc337bd175
Michal Maruska <mmaruska@gmail.com>
2010-06-08 23:31:23 +0000

    Expose ADD_PLUGIN call into (server) header file.
    So that the plugin modules can register itself.

--- include/xkbsrv.h
+++ include/xkbsrv.h
@@ -872,4 +872,14 @@ extern _X_EXPORT XkbDescPtr XkbCompileKeymap(DeviceIntPtr /* dev */ ,
                                              XkbRMLVOSet *      /* rmlvo */
     );
 
+
+#if MMC_PIPELINE
+/* this is called from the module to register/provide! */
+void xkb_add_plugin_class(DevicePluginRec* plugin);
+
+/* When a plugin is ready to be removed from the pipeline, it does so by calling: */
+Bool xkb_remove_plugin(PluginInstance* plugin);
+#endif
+
+
 #endif                          /* _XKBSRV_H_ */
