9da28315a5062be8f94823c94acc740a0620adee
Michal Maruska <mmaruska@gmail.com>
2010-06-09 22:58:52 +0000

    Extend MI calls, so that event processing can be related to "current time" Extend the system of Block/Wake-handlers, to use the "current" time.
    at wake it (the "current" time) is the upper bound of time for which
    all antecedent events have been read (from file descriptors).
    
    In the Block handler the time is current time (before we enter the select() call).
    Extend MI calls, so that event processing can be related to "current time"

--- mi/mieq.c
+++ mi/mieq.c
@@ -572,6 +572,12 @@ mieqProcessDeviceEvent(DeviceIntPtr dev, InternalEvent *event, ScreenPtr screen)
 
 /* Call this from ProcessInputEvents(). */
 void
+mieqProcessInputEventsTime(Time now)
+{
+    mieqProcessInputEvents();
+}
+
+void
 mieqProcessInputEvents(void)
 {
     EventRec *e = NULL;
