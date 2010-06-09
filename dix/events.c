6cfd063d7c6f1eee70fbcd76c4387629feb2b697
Michal Maruska <mmaruska@gmail.com>
2010-06-09 22:07:02 +0000

    comments on sync replaying

--- dix/events.c
+++ dix/events.c
@@ -1342,6 +1342,7 @@ ComputeFreezes(void)
       }
 
     syncEvents.playingEvents = TRUE;
+    /* mmc: this is only for replaying the 1 event which caused Grab */
     if (replayDev) {
         InternalEvent *event = replayDev->deviceGrab.sync.event;
 
@@ -1377,6 +1378,8 @@ ComputeFreezes(void)
             }
         }
     }
+    /* mmc: find the first/_any_ device not frozen ...
+     * fixme: This should run only on devs which were frozen up to now!   */
     for (dev = devices; dev; dev = dev->next) {
             if (!dev->deviceGrab.sync.frozen) {
 #if MMC_PIPELINE
