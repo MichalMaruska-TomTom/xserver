56c0e5b6b4542fff4bed4047745830a3a0079328
Michal Maruska <mmaruska@gmail.com>
2011-01-01 23:34:05 +0200

    verbosity on deactivating grab (allow-event replay is culprit)

--- dix/events.c
+++ dix/events.c
@@ -1661,7 +1661,7 @@ DeactivateKeyboardGrab(DeviceIntPtr keybd)
         : keybd->spriteInfo->sprite->win;
     Bool wasImplicit = (keybd->deviceGrab.fromPassiveGrab &&
                         keybd->deviceGrab.implicitGrab);
-
+    ErrorF("%s, now deactivating the grab!\n", __FUNCTION__);
     if (focusWin == FollowKeyboardWin)
         focusWin = inputInfo.keyboard->focus->win;
     if (keybd->valuator)
@@ -1820,6 +1820,7 @@ AllowSome(ClientPtr client, TimeStamp time, DeviceIntPtr thisDev, int newState)
                 grabinfo->sync.other = NullGrab;
             syncEvents.replayDev = thisDev;
             syncEvents.replayWin = grabinfo->grab->window;
+            ErrorF("%s, now deactivating the grab (Replay)!\n", __FUNCTION__);
             (*grabinfo->DeactivateGrab) (thisDev);
             syncEvents.replayDev = (DeviceIntPtr) NULL;
         }
@@ -2951,6 +2952,7 @@ ActivateFocusInGrab(DeviceIntPtr dev, WindowPtr old, WindowPtr win)
             IsParent(dev->deviceGrab.grab->window, win))
             return FALSE;
         DoEnterLeaveEvents(dev, dev->id, old, win, XINotifyPassiveUngrab);
+        ErrorF("%s, now deactivating the grab!\n", __FUNCTION__);
         (*dev->deviceGrab.DeactivateGrab) (dev);
     }
 
@@ -2991,6 +2993,7 @@ ActivateEnterGrab(DeviceIntPtr dev, WindowPtr old, WindowPtr win)
             IsParent(dev->deviceGrab.grab->window, win))
             return FALSE;
         DoEnterLeaveEvents(dev, dev->id, old, win, XINotifyPassiveUngrab);
+        ErrorF("%s, now deactivating the grab!\n", __FUNCTION__);
         (*dev->deviceGrab.DeactivateGrab) (dev);
     }
 
