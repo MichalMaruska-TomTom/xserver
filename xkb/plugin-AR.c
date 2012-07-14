f787765ccec62c79493c7475c63530dbe72a63bb
Michal Maruska <mmaruska@gmail.com>
2012-07-15 00:30:43 +0200

    don't enable debug AR.

--- xkb/plugin-AR.c
+++ xkb/plugin-AR.c
@@ -12,8 +12,8 @@
 
 /* If I could get the repeat-rate w/o using XKB... */
 
-/* 1 important, 2 burocracy, 4 all invocations */
-#define DEBUG_AUTOREPEAT 1
+/* 1 important, 2 burocracy, 4 all invocations; full: (1 | 2 | 4) */
+#define DEBUG_AUTOREPEAT 0
 
 #include <X11/extensions/XKBsrv.h>
 
