3582989961d81a693729b257e2c1bfa2b1eaae10
Michal Maruska <mmaruska@gmail.com>
2014-10-10 06:44:01 +0200

    Extend the system of Block/Wake-handlers, to use the "current" time.
    at wake it (the "current" time) is the upper bound of time for which
    all antecedent events have been read (from file descriptors).
    
    In the Block handler the time is current time (before we enter the select() call).

--- include/dix.h
+++ include/dix.h
@@ -136,6 +136,12 @@ typedef struct _TimeStamp {
     CARD32 milliseconds;
 } TimeStamp;
 
+/* BlockHandlerProcPtr is typedef'd in x11proto/Xdefs.h
+ * This version accepts the current time. */
+typedef void (* TimeBlockHandlerProcPtr)(void * /* blockData */,
+					 OSTimePtr /* pTimeout */,
+					 void * /* pReadmask */,
+					 Time /* current time*/);
 /* dispatch.c */
 
 extern _X_EXPORT void SetInputCheck(HWEventQueuePtr /*c0 */ ,
@@ -203,11 +209,25 @@ extern _X_EXPORT int AlterSaveSetForClient(ClientPtr /*client */ ,
 
 extern _X_EXPORT void DeleteWindowFromAnySaveSet(WindowPtr /*pWin */ );
 
-extern _X_EXPORT void BlockHandler(void */*pTimeout */ ,
-                                   void */*pReadmask */ );
+/* Possible BlockHandler has some points in time and when asked for a timeout,
+ * it * needs to compare the current time with those points to return the
+ * timeout. Therefore WaitForSomething has to provide us the current time. */
 
+extern _X_EXPORT void BlockHandler(void * /*pTimeout */ ,
+                                   void * /*pReadmask */ ,
+                                   Time /* now */);
+
+/* When woken by kernel, we need to know some time of the wakeup:
+   Time of last event or (guaranteed) non event. */
 extern _X_EXPORT void WakeupHandler(int /*result */ ,
-                                    void */*pReadmask */ );
+                                    void * /*pReadmask */,
+                                    Time /* now */);
+
+typedef void (* TimeWakeupHandlerProcPtr)(
+    void * /* blockData */,
+    int /* result */,
+    void * /* pReadmask */,
+    Time /* now */);
 
 void
  EnableLimitedSchedulingLatency(void);
@@ -224,6 +244,12 @@ extern _X_EXPORT Bool RegisterBlockAndWakeupHandlers(BlockHandlerProcPtr
                                                      WakeupHandlerProcPtr
                                                      /*wakeupHandler */ ,
                                                      void */*blockData */ );
+
+
+extern Bool RegisterTimeBlockAndWakeupHandlers(TimeBlockHandlerProcPtr /*blockHandler*/,
+                                               TimeWakeupHandlerProcPtr /*wakeupHandler*/,
+                                               void * /*blockData*/);
+
 
 extern _X_EXPORT void RemoveBlockAndWakeupHandlers(BlockHandlerProcPtr
                                                    /*blockHandler */ ,
